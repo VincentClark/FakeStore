712c98257b9d59d7de1e05e8c149ef12762a4584
Vincent Clark <vincentclark@gmail.com>
2021-08-05 15:26:25 -0700

    updated videoplayer nav to use style sheets instead of inline styles

--- fakestore-videoplayer/src/Components/NavBar.js
+++ fakestore-videoplayer/src/Components/NavBar.js
@@ -1,17 +1,15 @@
 import NavButton from "./Nav_Button";
-
+// REMOE COMMENTS ONCE SECURED
+//need to undo this and put everything in to css file
+//   color,  hoverColor,  backgroundColor,  hoverBackgroundColor,
+// bgColor
 const NavBar = ({
-  navConfig,
-  bgColor,
-  color,
-  hoverColor,
-  backgroundColor,
-  hoverBackgroundColor,
+  navConfig
 }) => {
   return (
     <nav>
-      <div className="menu" style={{ backgroundColor: bgColor }}>
-        <ul>
+      <div className="menu">
+        <ul className="menu_list">
           {navConfig.map((ELEMENT) => (
             <li key={ELEMENT.id.toString() + "li"} id={ELEMENT.id.toString() + "lid"}>
               <NavButton
@@ -19,10 +17,7 @@ const NavBar = ({
                 id={ELEMENT.key}
                 title={ELEMENT.title}
                 useRef={ELEMENT.ref}
-                color={color}
-                hoverColor={hoverColor}
-                backGroundColor={backgroundColor}
-                hoverBackgroundColor={hoverBackgroundColor}
+
               />
             </li>
           ))}
@@ -32,20 +27,20 @@ const NavBar = ({
   );
 };
 
-NavBar.defaultProps = {
-  navConfig: [
-    {
-      id: 1,
-      title: "Home",
-      ref: "/",
-      component: "HompePage",
-    }
-  ],
-  color: "white",
-  hoverColor: "rgb(240, 240, 240)",
-  backGroundColor: "rgb(70, 70, 70)",
-  hoverBackgroundColor: "rgb(61, 61, 61)",
-  bgColor: "rgb(70,70,70)",
-};
+// NavBar.defaultProps = {
+//   navConfig: [
+//     {
+//       id: 1,
+//       title: "Home",
+//       ref: "/",
+//       component: "HompePage",
+//     }
+//   ],
+//   color: "white",
+//   hoverColor: "rgb(240, 240, 240)",
+//   backGroundColor: "rgb(70, 70, 70)",
+//   hoverBackgroundColor: "rgb(61, 61, 61)",
+//   bgColor: "rgb(70,70,70)",
+// };
 
 export default NavBar;
--- fakestore-videoplayer/src/Components/Nav_Button.js
+++ fakestore-videoplayer/src/Components/Nav_Button.js
@@ -1,36 +1,36 @@
-import React, {useState} from "react";
-const Nav_Button = ({useRef, title, id, color,hoverColor,backGroundColor,hoverBackgroundColor}) => {
-    const [fontColor, setFontColor] = useState (color);
-    const [background,setBackground] = useState(backGroundColor);
-    function setColors(fColor,bColor){
-        setFontColor(fColor);
-        setBackground(bColor);
-    }
-    
-    const appStyles = {
-        color: `${fontColor}`,
-        backgroundColor: `${background}`
-        
-    };
-    
+// import React, { useState } from "react";
+const Nav_Button = ({ useRef, title, id }) => {
+    //backGroundColor, hoverBackgroundColor (removed props)
+    // const [fontColor, setFontColor] = useState(color);
+    // const [background, setBackground] = useState(backGroundColor);
+    // function setColors(fColor, bColor) {
+    //     setFontColor(fColor);
+    //     setBackground(bColor);
+    // }
+
+    // const appStyles = {
+    //     color: `${fontColor}`,
+    //     backgroundColor: `${background}`
+
+    // };
+    //style moved into css file
+    // will remove this once css is in place
     return (
         <a
-        style={appStyles}
-        onMouseEnter = {() => setColors(hoverColor,hoverBackgroundColor)}
-        onMouseLeave = {() => setColors(color,backGroundColor)}
-        id={id} 
-        href={useRef}
+            className="nav-button"
+            id={id}
+            href={useRef}
         >
-        {title}
+            {title}
         </a>
     );
 };
 
-Nav_Button.defaultProps = {
-    color: 'white',
-    hoverColor: 'rgb(253, 253, 253)',
-    backGroundColor: 'rgb(70, 70, 70)',
-    hoverBackgroundColor: 'rgb(61, 61, 61)'
-};
+// Nav_Button.defaultProps = {
+//     color: 'white',
+//     hoverColor: 'rgb(253, 253, 253)',
+//     backGroundColor: 'rgb(70, 70, 70)',
+//     hoverBackgroundColor: 'rgb(61, 61, 61)'
+// };
 
 export default Nav_Button;
