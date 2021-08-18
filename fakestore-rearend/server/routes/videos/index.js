471ce28630d1bb25c10d9d8c74705e73a0590802
Vincent Clark <vincentclark@gmail.com>
2021-08-18 04:47:24 -0700

    organizing upload. still need to fix 2 files not being removed from upload

--- fakestore-rearend/server/routes/videos/index.js
+++ fakestore-rearend/server/routes/videos/index.js
@@ -189,21 +189,11 @@ module.exports = (params) => {
         let default_poster = false;
         //this can be handled better. 
         try {
-            const video_file = req.files.video[0];
-            console.log("/videoupload");
             // get the base name of video_file
-            const file_name_base = video_file.originalname.split('.')[0];
             // let icon_fileName = "default_icon.png";
             // let poster_fileName = "default_poster.png";
-
             // get the extension of poster_file
-            console.log("req.files.icon[0]", req.files.icon[0])
-            let icon_file = req.files.icon[0];
             // get the base name of icon_file
-            const icon_extension = icon_file.originalname.split('.')[1];
-            let icon_fileName = file_name_base + "_icon" + "." + icon_extension;
-            console.log("icon_fileName", icon_fileName);
-
             // } else {
             //     default_icon = true;
             //     const icon_file = "defacult_icon.png"
@@ -213,43 +203,29 @@ module.exports = (params) => {
             // const icon_file = req.files.icon[0];
             // if (req.files.poster[0]) {
             //need to handle if there is no file designated. 
-            const poster_file = req.files.poster[0];
-            const poster_extension = poster_file.originalname.split('.')[1];
-            const poster_fileName = file_name_base + "_poster" + "." + poster_extension;
             // } else {
             //     default_poster = true;
             //     const poster_file = "default_poster.png"
             //     const poster_fileName = "default_poster.png";
             // }
-            console.log("icon_fileName NOW IS:" + icon_fileName);
-            const video_fileName = video_file.originalname;
-            const video_filePath = filepath_base + video_fileName;
-            const icon_filePath = filepath_base + icon_fileName;
-            const poster_filePath = filepath_base + poster_fileName;
-
-            const fileStream = fs.createWriteStream(video_filePath, { flags: 'w' },);
-            req.pipe(fileStream);
-            if (!default_icon) {
-
-                const iconStream = fs.createWriteStream(icon_filePath, { flags: 'w' },);
-                req.pipe(iconStream);
-            }
-            if (!default_poster) {
-                const posterStream = fs.createWriteStream(poster_filePath, { flags: 'w' },);
-                req.pipe(posterStream);
-            }
             req.on('end', () => {
                 fileStream.close();
                 console.log("PANIC FILE STREAM CLOSED");
                 //needed logic to get the file name
             });
-            let target_video_file = video_file.path;
-
-
-            console.log("target_video_file", target_video_file);
             //make this into a function when on service layer
-
             //ONLY SHOULD BE AVIALBLE IF UPLOADS ARE COMPLETE
+            //video upload start
+            const video_file = req.files.video[0];
+            const video_fileName = video_file.originalname;
+            //used for the icon and poster files
+            const file_name_base = video_file.originalname.split('.')[0];
+            const video_filePath = filepath_base + video_fileName;
+            const fileStream = fs.createWriteStream(video_filePath, { flags: 'w' },);
+            // should this be in a buffr? 
+            req.pipe(fileStream);
+            let target_video_file = video_file.path;
+            // console.log("target_video_file", target_video_file);
             fs.rename(target_video_file, `${filedest_video}${video_fileName}`, (err) => {
                 if (err) {
                     console.log("ERROR", err);
@@ -258,8 +234,16 @@ module.exports = (params) => {
                     console.log("rename successful {0}");
                 }
             });
+            //video upload end
+            //icon upload start
+            let icon_file = req.files.icon[0];
+            const icon_extension = icon_file.originalname.split('.')[1];
+            let icon_fileName = file_name_base + "_icon" + "." + icon_extension;
+            const icon_filePath = filepath_base + icon_fileName;
+            const iconStream = fs.createWriteStream(icon_filePath, { flags: 'w' },);
+            req.pipe(iconStream);
             let target_icon_file = icon_file.path;
-            fs.rename(target_icon_file, `${filedest_images}${icon_fileName}`, (err) => {
+            const rename = fs.rename(target_icon_file, `${filedest_images}${icon_fileName}`, (err) => {
                 if (err) {
                     console.log("ERROR", err);
                     console.log("video_filePath", icon_fileName);
@@ -267,55 +251,135 @@ module.exports = (params) => {
                     console.log("rename successful {1}");
                 }
             });
+            console.log("end icon upload");
+            //icon upload end
+            //poster upload start
+            //poster upload
 
-            let target_poster_file = poster_file.path;
-            fs.rename(target_poster_file, `${filedest_images}${poster_fileName}`, (err) => {
-                if (err) {
-                    console.log("ERROR", err);
-                    console.log("video_filePath", poster_filePath);;
-                } else {
-                    console.log("rename successful {2}");
+            const poster_file = req.files.poster[0];
+
+
+            // const poster_filePath = filepath_base + poster_fileName;
+            //const final_poster_path = `${filedest_images}${poster_fileName}`
+            console.log("CREATING POSTER FILE");
+            //NEEDS TO BE ASNCH?
+            const createPosterFile = (reqPosterFile) => {
+                console.log("createPosterFile");
+                if (reqPosterFile) {
+                    const poster_extension = poster_file.originalname.split('.')[1];
+                    const poster_fileName = file_name_base + "_poster" + "." + poster_extension;
+                    console.log("if statemen in createPoserFile");
+                    //const poster_file = reqPosterFile;
+                    console.log("reqPosterFile", reqPosterFile);
+
+
+                    try {
+                        const stream = fs.createWriteStream(reqPosterFile.path, { flags: 'w' },)
+                        console.log("posterStream", path)
+                        req.pipe(stream);
+                        const final_poster_path = `${filedest_images}${poster_fileName}`
+                        let target_poster_file = poster_file.path;
+                        fs.rename(target_poster_file, final_poster_path, (err) => {
+                            if (err) {
+                                console.log("ERROR", err);
+                                console.log("video_filePath", poster_filePath);
+
+                                return "default_poster.png"
+                            } else {
+                                console.log("rename successful {2}");
+                                console.log(poster_fileName);
+                                return poster_fileName;
+                            }
+                        });
+
+                    } catch (err) {
+                        console.log("ERROR WRITING STREAM FOR POSTER", err);
+                    }
+
+                    // console.log("posterStream", posterStream);
+                    // const postercreate = posterStream(reqPosterFile.path);
+                    // let poster_fileName = `${file_name_base}_poster.${poster_extension}`;
+                    // const target_poster_file = reqPosterFile.path;
+                    // const final_poster_path = `${filedest_images}${poster_fileName}`
+                    // try {
+
+                    //     // fs.rename(target_poster_file, final_poster_path, (err) => {
+                    //     //     if (err) {
+                    //     //         console.log("ERROR", err);
+                    //     //         console.log("video_filePath", poster_filePath);;
+                    //     //         return "default_poster.png"
+                    //     //     } else {
+                    //     //         console.log("rename successful {2}");
+                    //     //         console.log(poster_fileName);
+                    //     //         return poster_fileName;
+                    //     //     }
+                    //     // })
+
+
+                    // }
+                    // catch (err) {
+                    //     console.log("ERROR Poster File", err);
+                    //     return ("default_poster.png");
+                    // }
+
+
                 }
-            });
+                else {
+                    console.log("else statement in createPosterFile");
+                    return ("default_poster.png");
+                }
+
+            }
+            console.log("Step 1");
+            const posterFileName = createPosterFile(req.files.poster[0])
+
+            console.log("posterFileName", posterFileName);
+            //end poser upload
+
+
+
+
+
             //create a stub for the video
             //copiolet the file name to the stub
             // this is what goes into the DB, but the structure isn't right.
             // will need to readjust this once working.
             console.log("Working on DB portion");
-            try {
-                console.log("Started DB portion");
-                let creator = "vincent"
-                const stub = new stubModel({
-                    "title": video_fileName,
-                    "video_id": video_fileName + creator,
-                    "icon": icon_fileName,
-                    "src": file_name_base,
-                    "poster": poster_fileName,
-                    "creator": "Vincent NEED TO FIX",
-                    "description": "Haven't gotten that far dude",
-                    "defaultControls": true,
-                    "url": "",
-                    "service": "local",
-                    upvotes: 0,
-                    tags: ['cool', 'funny', 'awesome'],
-                    "views": 0,
-                    "comments": [],
-                }
-                );
-                stub.save((err) => {
-                    if (err) {
-                        console.log("ERROR", err);
-                    } else {
-                        console.log("Stub saved");
-                    }
-                }
-                );
-            } catch (err) {
-                console.log("ERROR IN 2nd TRY / routes/video/index")
-                console.log("remove video", "or should it be vice versa");
-            }
+            // try {
+            //     console.log("Started DB portion");
+            //     let creator = "vincent"
+            //     const stub = new stubModel({
+            //         "title": video_fileName,
+            //         "video_id": video_fileName + creator,
+            //         "icon": icon_fileName,
+            //         "src": file_name_base,
+            //         "poster": poster_fileName,
+            //         "creator": "Vincent NEED TO FIX",
+            //         "description": "Haven't gotten that far dude",
+            //         "defaultControls": true,
+            //         "url": "",
+            //         "service": "local",
+            //         upvotes: 0,
+            //         tags: ['cool', 'funny', 'awesome'],
+            //         "views": 0,
+            //         "comments": [],
+            //     }
+            //     );
+            //     // stub.save((err) => {
+            //     //     if (err) {
+            //     //         console.log("ERROR", err);
+            //     //     } else {
+            //     //         console.log("Stub saved");
+            //     //     }
+            //     // }
+            //     // );
+            // } catch (err) {
+            //     console.log("ERROR IN 2nd TRY / routes/video/index")
+            //     console.log("remove video", "or should it be vice versa");
+            // }
 
             res.status(200).json({ message: "video uploaded successfully" });
+
         }
         catch (err) {
             console.log('TRY: route/index', err);
