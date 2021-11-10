5d5b421ca87989bfd14b0aa2dd78788e1bcbb750
vincentclark <vincentclark@gmail.com>
2021-11-10 10:05:13 -0800

    minor updates to twilio version.

--- fakestore-rearend/server/config/index.js
+++ fakestore-rearend/server/config/index.js
@@ -11,5 +11,10 @@ module.exports = {
             hold: "Hold",
 
         }
+    },
+    account: {
+        TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+        TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+        TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER,
     }
 }
--- fakestore-rearend/server/service/TwilioService.js
+++ fakestore-rearend/server/service/TwilioService.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const request = require('request');
-
+const twilio = require('twilio');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 class TwilioService {
 
 }
